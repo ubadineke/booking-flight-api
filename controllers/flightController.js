9cb87e37dc8f17e4b45653e20560c69ec9c6c2e6
Prince <ubadinekeprince@gmaul.comgit config --global user.email ubadinekeprince@gmail.com>
2022-06-26 05:26:49 +0100

    Redone

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -3,4 +3,18 @@ exports.example = (req, res) => {
     res.send("Flight example")
 }
 
+const { v4: uuid } = require("uuid");
 
+let flightDetails = [
+  {
+    title: "flight to canada",
+    time: "1pm",
+    price: 26000,
+    date: "26-06-2022",
+    id: uuid(),
+  },
+];
+
+exports.getFlights = (req, res) => {
+    res.json(flightDetails);
+}
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
 router.get('/', controller.example)
 
-module.exports = router;
+//Get All Flights
+router.get('/allflights', controller.getFlights);
 
+//Get Single Flight
+//router.get('/:id', controller.singleFlight)
+
+//Add/Book A Flight
+//router.post('/bookflight', controller.bookFlight);
+
+//Update Existing Flight
+//router.put('updateflight/:id', controller.updateFlight)
+
+//Delete Flight
+//router.delete('deleteflight/:id', controller.deleteFlight)
+
+//Listen for Requests
+
+module.exports = router;
